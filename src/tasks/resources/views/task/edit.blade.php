b9b60cf996c86747d68ca0ff322027ae2e438cfe
t.sakagami <t_sakagami@miraino-katachih2.co.jp>
2019-12-05 21:14:06 +0900

    編集画面のバリデーションエラー時のフォームのデフォルト値を修正

--- src/tasks/resources/views/task/edit.blade.php
+++ src/tasks/resources/views/task/edit.blade.php
@@ -13,7 +13,13 @@
                     @if ($errors->has('task_name'))
                         <p class="warning">{{ $errors->first('task_name') }}</p>
                     @endif
-                    <input type="text" name="task_name" class="form-control task-name" value="{{ $task->task_name }}">
+                    @php
+                        $value = $task->task_name;
+                        if (old('task_name')) {
+                            $value = old('task_name');
+                        }
+                    @endphp
+                    <input type="text" name="task_name" class="form-control task-name" value="{{ $value }}">
                 </td>
             </tr>
             <tr>
@@ -22,17 +28,28 @@
                     @if ($errors->has('expire_date'))
                         <p class="warning">{{ $errors->first('expire_date') }}</p>
                     @endif
+                    @php
+                        $value = $task->expire_date;
+                        if (old('expire_date')) {
+                            $value = old('expire_date');
+                        }
+                    @endphp
                     <input type="date" name="expire_date" class="form-control expire-date" value="{{ $task->expire_date }}">
                 </td>
             </tr>
             <tr>
                 <th>完了</th>
-                <td><input type="checkbox" name= "finished_date" value="1" @if (!is_null($task->finished_date)) checked @endif
-                ></td>
+                @php
+                    $checked = '';
+                    if (old('expire_date') || $task->finished_date) {
+                        $checked = ' checked';
+                    }
+                @endphp
+                <td><input type="checkbox" name= "finished_date" value="1"{{ $checked }}></td>
             </tr>
         </table>
         <input type="submit" class="btn btn-primary" value="更新">
-        <input type="button" class="btn btn-primary" value="キャンセル" onclick="history.back();">
+        <input type="button" class="btn btn-primary" value="キャンセル" onclick="location.href='/task';">
     </form>
 
 @endsection
